a1e00edd38f381c70b2b2e93fbb210911099848a
nicholsjdm <168163298+nicholsjdm@users.noreply.github.com>
2024-05-22 05:51:21 -0500

    Update accessAPI.py

--- accessAPI.py
+++ accessAPI.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def get_ch_summary():
+def get_ch_summary(book, summary):
   base_url = 'https://openscriptureapi.org/api/scriptures/v1/lds/en/volume/bookofmormon/'
 
   # User input URL using variables below
@@ -21,16 +21,15 @@ def main():
 
   # Create a while loop for multiple entries
   while True:
-  
     # Ask for user input
-    book = input("Select a book from the Book of Mormon: ".replace(" ", "-").lower() # Replace spaces with hyphens and convert to lowercase
+    book = input("Select a book from the Book of Mormon: ").replace(" ", "-").lower() # Replace spaces with hyphens and convert to lowercase
     chapter = input("Select a chapter from the book you selected: ")
 
     # Call the function above and assign it a variable
     summary = get_ch_summary(book, chapter)
-
+    
     # Print Result
-    print(f"Summary of (book.title()} chapter {chapter}:")
+    print(f"Summary of {book.title()} chapter {chapter}:")
     print(summary)
 
     # Ask user if they want to repeat
